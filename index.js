4ed8c6bbf7d170aebeab235e399dae9392aa4682
Zachary Anderson <andersonzach02@gmail.com>
2020-07-16 13:40:32 -0400

    Did a simple, hardcoded first iteration of the creation of a Koch Fractal (that will eventually be repeated on the segments of an equilateral triangle to form a snowflake shape

--- index.js
+++ index.js
@@ -1,9 +1,38 @@
+let startVector;
+let endVector;
+let zero;
+let one;
+let two;
+let three;
+
 function setup() {
     createCanvas(400, 400);
+
+    startVector = createVector(0, height/2);
+    endVector = createVector(width, height/2);
+
+    let newVector = p5.Vector.sub(endVector, startVector);
+
+    newVector.div(3);
+
+    zero = p5.Vector.add(startVector, newVector);
+    two = p5.Vector.sub(endVector, newVector);
+
+    newVector.rotate(-PI/3);
+
+    one = p5.Vector.add(zero, newVector);
+    
 
 }
 
 
 function draw() {
     background(0);
+
+    stroke(255);
+    line(startVector.x, startVector.y, zero.x, zero.y);
+    line(zero.x, zero.y, one.x, one.y);
+    line(one.x, one.y, two.x, two.y);
+    line(two.x, two.y, endVector.x, endVector.y);
+
 }
